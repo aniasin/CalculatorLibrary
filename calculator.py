f06ac46b0cb006b5c143b1d24b923f7ed13421d3
DESKTOP-1F7422C\sillikone <pierrat.laurent@gmail.com>
2021-07-17 11:49:46 -0700

    Added unit test for calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,10 @@
-print("This is the calculator")
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
